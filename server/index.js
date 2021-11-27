58775fe116aa3102ed854f555439b5e59f50e2d1
Ruslan <rbe@crtweb.ru>
2021-11-27 14:32:30 +0500

    first

---
+++ server/index.js
@@ -0,0 +1,21 @@
+// read .env
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+const start = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+start()
